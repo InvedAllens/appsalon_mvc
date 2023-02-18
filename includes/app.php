a12161f1c9be550b2a197a23ee6908caa8b8eb2e
allens_aldo <56383926+InvedAllens@users.noreply.github.com>
2023-02-18 12:52:25 -0600

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -1,5 +1,5 @@
 <?php 
-    require '/app/'.__DIR__.'/../vendor/autoload.php';
+    require __DIR__.'/../vendor/autoload.php';
     $dotenv=Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->safeLoad();
 
